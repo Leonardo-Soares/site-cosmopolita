6d1a9f505d317e454627466493fb682702cf8bdb
Leonardo Soares <leonardoafonso1048@gmail.com>
2024-12-08 12:12:17 -0300

    feat(auth): ajustes finos de layout e responsividade

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import InputPrimary from '@/components/Forms/InputPrimary'
+import { Container } from '@/components/Partials/Container'
 import { ButtonPrimary } from '@/components/Buttons/ButtonPrimary'
 
 
@@ -10,45 +11,46 @@ export default async function Home() {
   return (
     <main className='flex-1 h-screen' style={{
       backgroundImage: 'url(../img/bg/bg-mapa-mudi.png)',
-      backgroundSize: 'cover',
+      backgroundSize: 'contain',
     }}>
-      <div className='max-w-[1080px] h-screen mx-auto flex justify-center items-center'>
-        <div className='grid grid-cols-1 lg:grid-cols-2 py-16 gap-x-12'>
-          <div className='order-2 mt-8 lg:mt-0 lg:order-1'>
-            <img className='w-full rounded-xl' src="../img/temp/login.png" alt="" />
-          </div>
-          <div className='flex flex-col justify-center order-1 lg:order-2'>
-            <div>
-              <h2 className='text-5xl font-bold text-brand-dark'>Cadastro</h2>
-              <p className='text-md text-brand-gray-700 mb-6'>Preenche todas informações com atenção, em caso de dúvida procure um Mestre Maçom ou membros da Diretoria da Loja.</p>
-
-              <InputPrimary name='name' placeholder='Nome' />
-              <InputPrimary name='date' type='date' placeholder='Data de nascimento' />
-              <InputPrimary name='email' placeholder='E-mail' type='email' />
-              <InputPrimary name='cim' placeholder='CIM' type='text' />
-              <InputPrimary name='endereco' placeholder='Endereço' type='text' />
-              <InputPrimary name='telefone' placeholder='Telefone' type='number' />
-              <InputPrimary name='password' placeholder='Senha' type='password' />
+      <div className='max-w-[1080px] h-screen mx-auto flex justify-center'>
+        <Container>
+          <div className='grid grid-cols-1 lg:grid-cols-2 py-16 gap-x-12 items-center'>
+            <div className='order-2 mt-8 lg:mt-0 lg:order-1 hidden lg:flex'>
+              <img className='w-full border-solid border-brand-blue border-4 rounded-xl' src="../img/temp/foto-exemplo.jpeg" alt="" />
             </div>
+            <div className='flex flex-col justify-center order-1 lg:order-2'>
+              <div>
+                <h2 className='text-5xl font-bold text-brand-dark'>Cadastro</h2>
+                <p className='text-md text-brand-gray-700 mb-6'>Preenche todas informações com atenção, em caso de dúvida procure um Mestre Maçom ou membros da Diretoria da Loja.</p>
 
-            <div className='flex-col flex'>
-              <ButtonPrimary
-                full={true}
-                onClick={() => console.log('Clicou')}
-              >
-                Entrar</ButtonPrimary>
-              <ButtonPrimary
-                full={true}
-                color='text-brand-blue'
-                backgroundColor='bg-transparent'
-                onClick={() => router.push('/')}
-              >
-                Fazer login</ButtonPrimary>
+                <InputPrimary name='name' placeholder='Nome' />
+                <InputPrimary name='date' type='date' placeholder='Data de nascimento' />
+                <InputPrimary name='email' placeholder='E-mail' type='email' />
+                <InputPrimary name='cim' placeholder='CIM' type='text' />
+                <InputPrimary name='endereco' placeholder='Endereço' type='text' />
+                <InputPrimary name='telefone' placeholder='Telefone' type='number' />
+                <InputPrimary name='password' placeholder='Senha' type='password' />
+              </div>
+
+              <div className='flex-col flex'>
+                <ButtonPrimary
+                  full={true}
+                  onClick={() => console.log('Clicou')}
+                >
+                  Entrar</ButtonPrimary>
+                <ButtonPrimary
+                  full={true}
+                  color='text-brand-blue'
+                  backgroundColor='bg-transparent'
+                  onClick={() => router.push('/')}
+                >
+                  Fazer login</ButtonPrimary>
+              </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
-
     </main>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation'
 import InputPrimary from '@/components/Forms/InputPrimary'
 import { ButtonPrimary } from '@/components/Buttons/ButtonPrimary'
 import useAuthenticated from '@/stores/useAuthenticated'
+import { Container } from '@/components/Partials/Container'
 
 export default async function Home() {
   const router = useRouter()
@@ -26,35 +27,38 @@ export default async function Home() {
       backgroundSize: 'cover',
     }}>
       <div className='max-w-[1080px] h-screen mx-auto flex justify-center items-center'>
-        <div className='grid grid-cols-1 lg:grid-cols-2 py-16 gap-x-12'>
-          <div className='order-2 mt-8 lg:mt-0 lg:order-1'>
-            <img className='w-full rounded-xl' src="../img/temp/login.png" alt="" />
-          </div>
-          <div className='flex flex-col justify-center order-1 lg:order-2'>
-            <div>
-              <h2 className='text-5xl font-bold text-brand-gray-700 mb-6'>Área Administrativa</h2>
-
-              <InputPrimary name='email' placeholder='E-mail' />
-              <InputPrimary name='password' placeholder='Senha' type='password' />
+        <Container>
+          <div className='grid grid-cols-1 lg:grid-cols-2 py-16 gap-x-12'>
+            <div className='order-2 mt-8 lg:mt-0 lg:order-1'>
+              <img className='w-full border-solid border-brand-blue border-4 rounded-xl' src="../img/temp/foto-exemplo.jpeg" alt="" />
             </div>
+            <div className='flex flex-col justify-center order-1 lg:order-2'>
+              <div>
+                <h2 className='text-5xl font-bold text-brand-gray-700 mb-6'>Área Administrativa</h2>
 
-            <div className='flex-col flex'>
-              <a onClick={() => router.push('/recuperar-senha')} className='text-brand-blue font-bold cursor-pointer hover:underline text-sm'>Esqueci minha senha</a>
-              <ButtonPrimary
-                full={true}
-                onClick={handleLogin}
-              >
-                Entrar</ButtonPrimary>
-              <ButtonPrimary
-                full={true}
-                color='text-brand-blue'
-                backgroundColor='bg-transparent'
-                onClick={() => router.push('/cadastro')}
-              >
-                Criar conta</ButtonPrimary>
+                <InputPrimary name='email' placeholder='E-mail' />
+                <InputPrimary name='password' placeholder='Senha' type='password' />
+              </div>
+
+              <div className='flex-col flex'>
+                <a onClick={() => router.push('/recuperar-senha')} className='text-brand-blue font-bold cursor-pointer hover:underline text-sm'>Esqueci minha senha</a>
+                <ButtonPrimary
+                  full={true}
+                  onClick={handleLogin}
+                >
+                  Entrar</ButtonPrimary>
+                <ButtonPrimary
+                  full={true}
+                  color='text-brand-blue'
+                  backgroundColor='bg-transparent'
+                  onClick={() => router.push('/cadastro')}
+                >
+                  Criar conta</ButtonPrimary>
+              </div>
             </div>
           </div>
-        </div>
+
+        </Container>
       </div>
 
     </main>
