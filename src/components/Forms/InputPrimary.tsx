4c59e16cecd0e011f50787a432979aa72c532cab
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2024-12-10 20:30:20 -0300

    chore(Cadastro): validação de formulário

--- src/components/Forms/InputPrimary.tsx
+++ src/components/Forms/InputPrimary.tsx
@@ -9,15 +9,17 @@ interface InputPrimaryProps {
   required?: boolean
   placeholder: string
   onChange?: any
+  value?: string
+  maxLength?: number
 }
 
-export default function InputPrimary({ name, type, title, required, placeholder, onChange }: InputPrimaryProps) {
+export default function InputPrimary({ name, type, title, maxLength, required, placeholder, value, onChange }: InputPrimaryProps) {
   const [senhaVisivel, setSenhaVisivel] = useState(false)
 
   return (
     <div className='relative'>
       {title &&
-        <label htmlFor={name} className="block text-md text-dark">
+        <label htmlFor={name} className="block text-md font-semibold text-dark">
           {title}
         </label>
       }
@@ -25,12 +27,15 @@ export default function InputPrimary({ name, type, title, required, placeholder,
         name={name}
         required={required}
         onChange={onChange}
+        value={value}
+        min={0}
+        maxLength={maxLength}
         placeholder={placeholder}
         type={type === 'password' && senhaVisivel ? 'text' : type}
-        className="w-full h-12 mb-2 pl-4 pr-10 border border-brand-gray-200 rounded-xl focus:outline-none focus:border-brand-blue focus:ring-1 focus:ring-brand-blue"
+        className="w-full font-semibold h-12 mb-2 pl-4 pr-10 border border-brand-gray-200 rounded-xl focus:outline-none focus:border-brand-blue focus:ring-1 focus:ring-brand-blue"
       />
       {type === 'password' &&
-        <a onClick={() => setSenhaVisivel(!senhaVisivel)} className='absolute z-50 cursor-pointer top-4 right-4'>
+        <a onClick={() => setSenhaVisivel(!senhaVisivel)} className={`absolute z-50 cursor-pointer ${title ? 'top-10 right-4' : 'top-4 right-4'} `}>
           {senhaVisivel ?
             <img src="../img/icons/icon-eye-not.svg" alt="" />
             :
