67eae373004ae58fd3af57de9f9dc9e29c15b1dc
Leonardo Soares <leonardoafonso1048@gmail.com>
2024-12-10 22:44:52 -0300

    feat(LGPD): page de política de privacidade

--- src/components/Header/TopBar.tsx
+++ src/components/Header/TopBar.tsx
@@ -5,7 +5,7 @@ export default function TopBar() {
   return (
     <div className="border-b py-1">
       <Container>
-        <Link href={'/'} className="text-brand-gray-700 hover:underline ml-2 text-xs">
+        <Link href={'/politica-de-privacidade'} className="text-brand-gray-700 hover:underline ml-2 text-xs">
           Política de Privacidade
         </Link>
       </Container>
