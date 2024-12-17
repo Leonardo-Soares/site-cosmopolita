f6366b9255795638127f4067595940c883e1a697
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2024-12-16 21:14:37 -0300

    feat(layout): responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
+import toast from 'react-hot-toast'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import InputPrimary from '@/components/Forms/InputPrimary'
-import { ButtonPrimary } from '@/components/Buttons/ButtonPrimary'
-import { Container } from '@/components/Partials/Container'
 import { useCookies } from '@/stores/useCookies'
-import toast from 'react-hot-toast'
+import InputPrimary from '@/components/Forms/InputPrimary'
+import { Container } from '@/components/Partials/Container'
+import { ButtonPrimary } from '@/components/Buttons/ButtonPrimary'
 
 export default function Home() {
   const router = useRouter()
